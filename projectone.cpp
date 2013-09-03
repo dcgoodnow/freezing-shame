464de5eaad7bec038fd40a86f6896ddd818af04a
Daniel <danielgoodnow@gmail.com>
2013-09-02 23:44:42 -0700

    added load deck, somewhat functional

--- projectone.cpp
+++ projectone.cpp
@@ -1,3 +1,4 @@
+
 /*****************************************
  *Name: Daniel Goodnow
  *
@@ -10,9 +11,11 @@
  *Last Updated: 8/30/13
 *****************************************/
 #include <iostream>
+#include <fstream>
 
-using namespace std
+using namespace std;
 
+const int CARD_LENGTH = 25;
 
 /***
 	*Name: PrintMenu
@@ -20,7 +23,7 @@ using namespace std
 	*Args: none
 	*Retval: none, prints menu 
 ***/
-void PrintMenu();
+void PrintMenu(); //foo
 
 /**
   *Name: 	LoadDeck
@@ -28,40 +31,44 @@ void PrintMenu();
   *Args: array for deck to be loaded into
   *Retval: loads array
 */
-void LoadDeck(unshuffledDeck[]);
+void LoadDeck(char unshuffledDeck[][25][CARD_LENGTH]);
 
 int main()
 {
-	char userResponse;
-	//display user menu
-	PrintMenu();
+   char unshuffled[5][25][CARD_LENGTH]; 
+   char userResponse;
+   //display user menu
+   PrintMenu();
 
-	//get user response
-	std::cin >> userResponse;
+   //get user response
+   cin >> userResponse;
+   
+      //respond to action
+      switch(userResponse)
+      {
+         case '1':
+	 {
+            cout << "working";
+	    break;
+	 }
 
-	//respond to action
-	switch(userResponse)
-	{
-		case('1')
-		{
-			break;
-		}
+         //read in deck
+         case '2':
+	    {
+               LoadDeck(unshuffled);
+               cout << unshuffled[0][1];
+	       break;
+	    }
 
-		//read in deck
-		case('2')
-		{
-			break;
-		}
+	 //shuffle deck
+         case '3':
+	 {
+	    break;
+	 }
+      }
 
-		//shuffle deck
-		case('3')
-		{
-			break;
-		}
-
-		//print deck
-
-	return 0;
+      cout << endl;
+      return 0;
 }
 
 void PrintMenu()
@@ -73,7 +80,23 @@ void PrintMenu()
 	cout << "3. Write shuffled deck to file" << endl;
 }
 
-void LoadDeck(unshuffledDeck[])
+void LoadDeck(char unshuffledDeck[][25][CARD_LENGTH])
 {
-	
+   //open file 
+   ifstream unoDeck;
+   unoDeck.open("cards.txt");
+   
+   for(int i = 0; i < 4; i++)
+   {
+      for( int j = 0; j < 25; j++)
+      {
+         unoDeck >> unshuffledDeck[i][j];
+         unoDeck >> unshuffledDeck[i][j];
+      }
+   }
+   for( int i = 0; i < 25; i++)
+   {
+      unoDeck >> unshuffledDeck[4][i];
+   }
+   unoDeck.close();
 }
