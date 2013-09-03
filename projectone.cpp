d4334c610da952108ad60c9651fe8392f44f7922
Daniel <danielgoodnow@gmail.com>
2013-09-02 20:19:24 -0700

    git add cards.txt

--- projectone.cpp
+++ projectone.cpp
@@ -11,6 +11,8 @@
 *****************************************/
 #include <iostream>
 
+using namespace std
+
 
 /***
 	*Name: PrintMenu
@@ -20,36 +22,58 @@
 ***/
 void PrintMenu();
 
+/**
+  *Name: 	LoadDeck
+  *Purpose:	loads unshuffled deck from a  file
+  *Args: array for deck to be loaded into
+  *Retval: loads array
+*/
+void LoadDeck(unshuffledDeck[]);
+
 int main()
 {
 	char userResponse;
 	//display user menu
 	PrintMenu();
+
 	//get user response
 	std::cin >> userResponse;
+
 	//respond to action
+	switch(userResponse)
+	{
+		case('1')
+		{
+			break;
+		}
 
 		//read in deck
+		case('2')
+		{
+			break;
+		}
 
 		//shuffle deck
+		case('3')
+		{
+			break;
+		}
 
 		//print deck
 
 	return 0;
 }
 
-/***
-	*Name: PrintMenu
-	*Purpose: Prints user menu
-	*Args: none
-	*Retval: none, prints menu 
-***/
 void PrintMenu()
 {
-	std::cout << "Welcome to Uno Shuffler!" << std::endl;
-	std::cout << "What would you like to do?" << std::endl;
-	std::cout << "1. Shuffle the deck" << std::endl;
-	std::cout << "2. Print unshuffled deck" << std::endl;
-	std::cout << "3. Write shuffled deck to file" << std::endl;
+	cout << "Welcome to Uno Shuffler!" << endl;
+	cout << "What would you like to do?" << endl;
+	cout << "1. Shuffle the deck" << endl;
+	cout << "2. Print unshuffled deck" << endl;
+	cout << "3. Write shuffled deck to file" << endl;
 }
 
+void LoadDeck(unshuffledDeck[])
+{
+	
+}
