28bdc5fd0e5b6c2e5bd6dd54cdb9aaf3dde02fa2
Daniel <danielgoodnow@gmail.com>
2013-09-30 20:36:21 -0700

    added string headers

---
+++ weekfive/string.h
@@ -0,0 +1,15 @@
+
+void StringCopy(const char*, char*);
+
+/**
+  *Name:    Shuffle Deck
+  *Purpose: moves the unshuffled deck to shuffled deck in random order
+  *Args:    array of 108 unshuffled cards, array of 108 cards to store shuffled deck
+  *Retval:  moves shuffled deck into shuffled array by pointer
+**/
+
+int length(char*);
+
+bool StringCompare(char*, char*);
+
+void StringConcat(char*, char*);
