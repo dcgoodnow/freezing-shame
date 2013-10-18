1cadc846c92f6c2feab2b53efed2ffd32f988c69
Daniel <danielgoodnow@gmail.com>
2013-10-18 14:39:38 -0700

    removed old function prototypes

--- weekseven/uno.h
+++ weekseven/uno.h
@@ -47,27 +47,7 @@ void ShuffleDeck(card*, card*);
 **/
 void WriteDeck(const card*, const char*);
 
-/*
- * Name:    Initialize Deck
- * Purpose: Initialize all cards in deck to generic values
- * Args:    array of 108 cards
-*/
-void InitializeDeck(card*);
 
-/**
-  *Name:    Initialize player
-  *Purpose: initializes player to generic values
-  *Args:    array of 4 players
-  *Retval:  none
-**/
-void InitializePlayer(player*, int);
-
-/*
- * Name:    Initialize hand 
- * Purpose: intializes hand for player
- * Args:    card pointer to hand 
-*/
-void InitializeHand(card*);
 
 /**
  * Name:    Load Players
@@ -76,12 +56,6 @@ void InitializeHand(card*);
 */
 void LoadPlayers(player* list, ifstream& players, int numplayers);
 
-/*
- * Name:    Print Player
- * Purpose: Prints all player information to console
- * Args:    Player struct to be printed
-*/
-void PrintPlayer(player);
 
 /*
  * Name:    Deal Cards
@@ -91,32 +65,5 @@ void PrintPlayer(player);
 */
 void DealCards(card*, card*, card*, player*, int);
 
-/*
- * Name:    Copy Card
- * Purpose: copies all card data from one card to another
- * Args:    card to copy from, card to be copied (by reference)
-*/
-void CopyCard(card, card&);
-
-/*
- * Name:    Delete deck
- * Purpose: deletes dynamically allocated memory of the cards
- * Args:    pointer to deck to delete
-*/
-void DeleteDeck(card*);
-
-/*
- * Name:    Delete players
- * Purpose: deletes dynamically allocated memory of players
- * Args:    pointer to player array
-*/
-void DeletePlayers(player*, int);
-
-/*
- * Name:    Print card
- * Purpose: prints all aspects of specified card
- * Args:    card to print
-*/
-void PrintCard(card);
 
 card* SortCardsColor(card*, int);
