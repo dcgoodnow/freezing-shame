8f46f930358fa771f02a9cdbf5afb3ad8fa4870e
Daniel <danielgoodnow@gmail.com>
2013-10-18 15:21:13 -0700

    testing file

---
+++ weekseven/test.cpp
@@ -0,0 +1,21 @@
+#include "uno.h"
+#include <iostream>
+#include "string.h"
+
+int main()
+{
+   card * loadc;
+   loadc = new card[108];
+   LoadDeck(loadc);
+   card* sorted = new card[108];
+   for(int i = 0; i < 108; i++)
+   {
+      loadc[i].print();
+   }
+   sorted = SortCardsColor(loadc, 108);
+   for(int i = 0; i < 108; i++)
+   {
+      sorted[i].print();
+   }
+   return 0;
+}
